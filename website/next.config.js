46e67a15382d901a58b16fa078733de86e3bd6d1
Chalo Salvador <chalo@monogram.io>
2022-09-26 19:44:08 -0500

    Remove comment

--- website/next.config.js
+++ website/next.config.js
@@ -6,12 +6,7 @@ module.exports = {
   async rewrites() {
     return {
       beforeFiles: [
-        // These rewrites are checked after headers/redirects
-        // and before all files including _next/public files which
-        // allows overriding page files.
-        // Wildcard rewrite to CORS file in Azure.
-
-        // anything pointing to <WEBSITE_URL>/portal/* will be rewritten to <PORTAL_URL>/portal/*
+        // Anything pointing to <WEBSITE_URL>/portal/* will be rewritten to <PORTAL_URL>/portal/*
         // including <WEBSITE_URL>/portal => <PORTAL_URL>/portal
         {
           source: `${process.env.NEXT_PUBLIC_PORTAL_BASE_PATH}/:path*`,
